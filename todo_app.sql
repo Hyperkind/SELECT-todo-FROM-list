a792f1a2b47ea8581870729cf79c5a1d4c2da2b0
Jonathan Ah-Nee <jonahnee@gmail.com>
2016-01-11 17:21:09 -1000

    completed up to v

---
+++ todo_app.sql
@@ -0,0 +1,51 @@
+-- 1 Query to delete michael
+DROP USER IF EXISTS michael;
+
+-- 2 Creating user michael
+CREATE USER michael ENCRYPTED PASSWORD 'stonebreaker';
+
+-- 3 Drops database named todo_app
+DROP DATABASE IF EXISTS todo_app;
+
+-- 4 Creates a new database named todo_app
+CREATE DATABASE todo_app;
+
+-- 5 Connect to the newly created database
+\c todo_app;
+
+-- 6 Create a new table named tasks
+CREATE TABLE tasks(
+  id serial,
+  title character varying(255),
+  description text null,
+  created_at timestamp without time zone,
+  updated_at timestamp without time zone null,
+  completed boolean
+);
+
+-- 7 Define column id as the table's primary key
+ALTER TABLE tasks
+ADD PRIMARY KEY (id);
+
+-- 8 Queries to accomplish the following
+-- i. remove the column name completed
+ALTER TABLE tasks
+DROP COLUMN IF EXISTS completed;
+-- ii. add a column to tasks named completed_at: timestamp,
+-- that may be NULL, and has a default value of NULL
+ALTER TABLE tasks
+ADD COLUMN completed_at timestamp NULL default NULL;
+-- iii. change the updated_at column to not allow NULL values,
+-- and have a default value of now()
+ALTER TABLE tasks
+  ALTER COLUMN updated_at SET NOT NULL;
+ALTER TABLE tasks
+  ALTER COLUMN updated_at SET DEFAULT NOW();
+-- iv. create a new task, only by setting values
+-- (not defining which columns)
+INSERT INTO tasks
+VALUES (DEFAULT, 'Study SQL', 'Complete this exercise', now(), now(), NULL);
+-- v create a new task
+INSERT INTO tasks (title, description)
+VALUES ('Study PostgreSQL', 'Read all the documentation');
+-- vi select all the titles of tasks that are not yet completed
